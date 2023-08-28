74e6206bc087278e0a0b2fbac5d9d8f9880bc61f
Youssef Meskini <youss.meskini@gmail.com>
2023-08-28 16:58:45 +0100

    init chakra ui router and vitest

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
+};
